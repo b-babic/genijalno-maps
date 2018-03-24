6e26eefb9eb545f73bcb70942d261e569d870fc7
kekebeen <kakosi69@gmail.com>
2018-03-24 16:58:39 +0100

    Added root reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+/*import auth from "./auth";
+import userData from "./userData";
+import adminData from "./adminData";*/
+import ui from "./ui";
+
+const rootReducer = combineReducers({
+  ui
+});
+
+export default rootReducer;
---
+++ client/src/reducers/ui.js
@@ -0,0 +1,22 @@
+const ui = (
+  state = {
+    leftPanel: "active",
+    rightPanel: ""
+  },
+  action
+) => {
+  switch (action.type) {
+    case "ACTIVATE_LEFT_PANEL":
+      return { ...state, leftPanel: "active" };
+    case "DEACTIVATE_LEFT_PANEL":
+      return { ...state, leftPanel: "" };
+    case "ACTIVATE_RIGHT_PANEL":
+      return { ...state, rightPanel: "active" };
+    case "DEACTIVATE_RIGHT_PANEL":
+      return { ...state, rightPanel: "" };
+    default:
+      return state;
+  }
+};
+
+export default ui;
