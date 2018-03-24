6c2120ea6be2206e0283c3a6b3e6ccfafa87ec99
kekebeen <kakosi69@gmail.com>
2018-03-24 16:11:35 +0100

    Seeder updates

--- server/seed.js
+++ server/seed.js
@@ -3,3 +3,19 @@ module.exports = {
 	"dev": "mongodb://localhost:27017/genijalno-user-status",
 	"prod": "mongodb://localhost:27017/genijalno-user-status"
 }
+
+// examples 
+// male
+/*
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/male01.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/male02.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/male03.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/male04.png
+*/
+// female
+/*
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/female01.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/female02.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/female03.png
+https://i1.sdlcdn.com/static/img/marketing-mailers/mailer/2016/upload999/female04.png
+*/
