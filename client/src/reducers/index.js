3725b356f2e9d3885b71514461d9dea848676a11
kekebeen <kakosi69@gmail.com>
2018-03-26 20:46:26 +0200

    Updated reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from "redux";
-/*import auth from "./auth";
-import userData from "./userData";
-import adminData from "./adminData";*/
+import selectedUser from "./selectedUser";
 import userData from "./userData";
 import auth from "./auth";
 import ui from "./ui";
 
 const rootReducer = combineReducers({
+  selectedUser,
   auth,
   userData,
   ui
---
+++ client/src/reducers/selectedUser.js
@@ -0,0 +1,12 @@
+const selectedUser = (state = { selected: false, activeData: "" }, action) => {
+  switch (action.type) {
+    case "SET_SELECTED_USER":
+      return { ...state, activeData: action.payload };
+    case "DELETE_SELECTED_USER":
+      return { ...state, activeData: "" };
+    default:
+      return state;
+  }
+};
+
+export default selectedUser;
