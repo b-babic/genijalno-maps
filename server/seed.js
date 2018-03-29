1032e4b2fb4a7ac837659bb98bd1f23df84355a6
kekebeen <kakosi69@gmail.com>
2018-03-29 15:11:56 +0200

    Seed file missing

---
+++ server/seed.js
@@ -0,0 +1,5 @@
+module.exports = {
+  undefined: "mongodb://localhost:27017/genijalno-user-status",
+  dev: "mongodb://localhost:27017/genijalno-user-status",
+  prod: "mongodb://localhost:27017/genijalno-user-status"
+};
